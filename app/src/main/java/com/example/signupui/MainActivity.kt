1e836dfccd8cb7eea536497588edce7c4cb94c8d
SamaAbdelghfar99 <samaabdelghfar90@gmail.com>
2024-03-19 04:22:25 +0200

    commit

---
+++ app/src/main/java/com/example/signupui/MainActivity.kt
@@ -0,0 +1,14 @@
+// Name: sama abdelghfar mostaafa
+// Group: g2
+package com.example.signupui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
